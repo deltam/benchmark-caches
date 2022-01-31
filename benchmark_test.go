6d61386fc86bf5a93f30e3fdf32dc18e6702039c
MISUMI Masaru <deltam@users.noreply.github.com>
2022-01-31 13:14:40 +0900

    Add benchmark ristretto, naive map

--- benchmark_test.go
+++ benchmark_test.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"testing"
 	"time"
 
 	cache4 "github.com/akyoto/cache"
+	cache5 "github.com/dgraph-io/ristretto"
 	cache2 "github.com/muesli/cache2go"
 	cache3 "github.com/patrickmn/go-cache"
 	cache1 "github.com/rif/cache2go"
@@ -84,6 +86,70 @@ func BenchmarkAkyotoCache(b *testing.B) {
 			_, ok := c.Get(key(i))
 			if !ok {
 				c.Set(key(i), 123, time.Minute)
+			}
+		}
+	})
+}
+
+func BenchmarkRistretto(b *testing.B) {
+	c, err := cache5.NewCache(&cache5.Config{
+		NumCounters: 1e7,     // number of keys to track frequency of (10M).
+		MaxCost:     1 << 30, // maximum cost of cache (1GB).
+		BufferItems: 64,      // number of keys per Get buffer.
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b.Run("Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			c.Set(key(i), 123, 1)
+		}
+	})
+
+	// wait for value to pass through buffers
+	c.Wait()
+
+	b.Run("Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			v, ok := c.Get(key(i))
+			if !ok {
+				_ = v
+			}
+		}
+	})
+}
+
+func BenchmarkNaiveMap(b *testing.B) {
+	type item struct {
+		val       interface{}
+		expiredAt time.Time
+	}
+	c := make(map[string]item, 1000)
+
+	b.Run("Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			c[key(i)] = item{123, time.Now().Add(time.Minute)}
+		}
+	})
+
+	Get := func(key string) (interface{}, bool) {
+		itm, ok := c[key]
+		if !ok {
+			return nil, false
+		}
+		if time.Now().After(itm.expiredAt) {
+			delete(c, key)
+			return nil, false
+		}
+		return itm.val, true
+	}
+
+	b.Run("Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			v, ok := Get(key(i))
+			if !ok {
+				_ = v
 			}
 		}
 	})
